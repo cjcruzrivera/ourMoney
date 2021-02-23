fd8e75d34995e6adc3d013061cf2b290dba8de40
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-02-23 15:53:48 -0500

    :art: create organiced structure.

---
+++ project/routes/main.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+# from flask_login import login_required, current_user
+# from .models import User, Calculo
+from ..extensions import db
+from ..models import User
+# from datetime import datetime
+# import json
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
