1f633ad61ea8fbd02dce510fada50340c475039e
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-02-23 15:54:45 -0500

    :building_construction: add flaskenv, command and pipfile

---
+++ project/commands.py
@@ -0,0 +1,23 @@
+import click
+from flask.cli import with_appcontext
+
+from .extensions import db
+from .models import User, Fuente, Transaccion
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.drop_all()
+    db.create_all()
+
+    admin = User(
+        email="cruz.camilo@correounivalle.edu.co",
+        password="",
+        username="admin",
+        nombre="Administrador",
+        apellidos="",
+        img="",
+        isAdmin=True
+    )
+    db.session.add(admin)
+    db.session.commit()
